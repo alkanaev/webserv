ecd864144d1a737abadea0ef1907011365b04a01
abaudot <aimebaudot@gmail.com>
2022-03-19 17:01:08 +0100

    just changed the default response for fuuuun

--- Client.hpp
+++ Client.hpp
@@ -18,7 +18,7 @@
 
 # define CLIENT_BUFFER_SIZE 4092
 # define CLIENT_TIMEOUT 60
-# define RESPONSE "HTTP/1.1 200 OK\r\n\r\n Hello Weeb World :D!!" /* juste proof of work */
+# define RESPONSE "HTTP/1.1 200 OK\r\n\r\n <HTML>\n<HEAD>\n<TITLE>Your Title Here</TITLE>\n""</HEAD>\n<BODY BGCOLOR=\"FFFFFF\">\n<CENTER><IMG SRC=\"clouds.jpeg\" ALIGN=\"BOTTOM\"> </CENTER>\n<HR>\n<a href=\"http://somegreatsite.com\">Link Name</a>\nis a link to another nifty site\n<H1>This is a Header</H1>\n<H2>This is a Medium Header</H2>\nSend me mail at <a href=\"mailto:support@yourcompany.com\">\nsupport@yourcompany.com</a>.\n<P> This is a new paragraph!\n<P> <B>This is a new paragraph!</B>\n<BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>\n<HR>\n</BODY>\n</HTML>\n"
 
 /* minimal Client class... */
 class Client
