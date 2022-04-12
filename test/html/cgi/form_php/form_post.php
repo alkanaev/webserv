adaa2fe8e4c25316c935645297cf8a14194cbd08
Alina <kalina9007@gmail.com>
2022-04-12 13:55:58 +0200

    py and php

---
+++ test/html/cgi/form_php/form_post.php
@@ -0,0 +1,4 @@
+***********************************************
+Hello hello dear <?php echo htmlspecialchars($_POST['first_name']); ?> <?php echo htmlspecialchars($_POST['last_name']); ?> ! 
+We wish you a great day <3
+***********************************************
